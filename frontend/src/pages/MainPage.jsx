3dbfa4ae0116502156dea8e9b4c1f7491cfd7453
Vladyslav Krykun <mr.laggermpc.890@gmail.com>
2025-12-21 22:16:13 +0200

    Init MainPage

---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import styles from './MainPage.module.css'
+
+function MainPage() {
+    return (
+        <div className={styles.MainPage}>
+            Hello
+        </div>
+    )
+}
+
+export default MainPage
